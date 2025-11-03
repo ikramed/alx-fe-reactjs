8521878b6a76a5862ca2e0c04f5bf94326556126
ikram <ikram22eddahby@gmail.com>
2025-11-03 22:04:53 +0100

    fix inline css

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,8 +1,10 @@
 const UserProfile = (props) => {
   return (
-    <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
       <h2 style={{ color: 'blue' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+      <p>
+        Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>
+      </p>
       <p>Bio: {props.bio}</p>
     </div>
   );
