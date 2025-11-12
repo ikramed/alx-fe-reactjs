74a456b710467668323f2f34ffdb7b8420abd519
ikram <ikram22eddahby@gmail.com>
2025-11-12 15:19:23 +0100

    add files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/add">Add Recipe</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
     </div>
   );
 }
+
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={id} />;
+};
 
 export default App;
---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,25 @@
+import { useRecipeStore } from './recipeStore';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
+
+const RecipeDetails = ({ recipeId }) => {
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === Number(recipeId))
+  );
+
+  if (!recipe) {
+    return <p>Recipe not found</p>;
+  }
+
+  return (
+    <div>
+      <h2>{recipe.title}</h2>
+      <p>{recipe.description}</p>
+
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
