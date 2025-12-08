d74c1f09b0a49ee2eb919b5b5dc651bc157de584
ikram <ikram22eddahby@gmail.com>
2025-12-08 23:04:26 +0100

    .

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
-function ProtectedRoute({ isAuth, children }) {
-  if (!isAuth) {
+function ProtectedRoute({ children }) {
+  const { user } = useAuth(); 
+
+  if (!user) {
     return <Navigate to="/login" replace />;
   }
   return children;
