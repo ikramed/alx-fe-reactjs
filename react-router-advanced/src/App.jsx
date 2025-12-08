9ba81054066c287da9689fcaaf04b3131823d0e9
ikram <ikram22eddahby@gmail.com>
2025-12-08 23:01:18 +0100

    .

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
-// استيراد المكونات
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
 import Login from "./components/Login";
-
-// مكون للحماية
-const ProtectedRoute = ({ isAuth, children }) => {
-  return isAuth ? children : <Navigate to="/login" />;
-};
+import Profile from "./components/Profile";
+import UserProfile from "./components/UserProfile";
+import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const isAuthenticated = false; 
+  const isAuth = true; 
 
   return (
     <Router>
@@ -22,15 +16,21 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
 
-       
+        {/* Protected Profile */}
         <Route
           path="/profile/*"
           element={
-            <ProtectedRoute isAuth={isAuthenticated}>
+            <ProtectedRoute isAuth={isAuth}>
               <Profile />
             </ProtectedRoute>
           }
         />
+
+        {/* Dynamic User */}
+        <Route path="/user/:userId" element={<UserProfile />} />
+
+        {/* Dynamic Blog */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
