8228d1d5e72adc7f98872bdac7ec22c72968a69a
ikram <ikram22eddahby@gmail.com>
2025-12-10 17:45:20 +0100

    Fix App.jsx to import TodoList component

--- react-todo/src/app.jsx
+++ react-todo/src/app.jsx
@@ -1,13 +1,11 @@
 // src/App.jsx
 import React from 'react';
 import TodoList from './TodoList';
-import AddTodoForm from './components/AddTodoForm';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Todo List</h1>
-      <AddTodoForm />
       <TodoList />
     </div>
   );
