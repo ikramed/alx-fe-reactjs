cfd9954d8574f2959863f2c4f56c070adb770d56
ikram <ikram22eddahby@gmail.com>
2025-12-10 17:42:40 +0100

    C:/Program Files/Git/

---
+++ react-todo/src/components/AddTodoForm.jsx
@@ -0,0 +1,28 @@
+// src/components/AddTodoForm.jsx
+import React, { useState } from 'react';
+
+function AddTodoForm({ addTodo }) {
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTodo.trim() !== '') {
+      addTodo(newTodo);
+      setNewTodo('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+export default AddTodoForm;
